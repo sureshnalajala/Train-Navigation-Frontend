66c1341a8d1364c25e4c87788f90aa68a078a55a
suresh <sureshnalajala3@gmail.com>
2024-12-05 11:35:34 -0500

    Vue.js Application

---
+++ suresh_vue/src/components/TrainDetailsBox.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="traindetailspage">
+        <div id="traindetailsbox">
+            <div v-for="train of trainDetails" :key="train.id" >
+                <trainDetails :train="train" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TrainDetails from './SingleTrainDetails.vue'
+
+export default {
+    name: 'TrainDetailsBox',
+    props:{
+        trainDetails: Array
+    },
+    components:{
+        TrainDetails
+    }    
+}
+</script>
+
+<style scoped>
+
+#traindetailsbox
+{
+    width:95%;
+    margin:0 auto;
+    padding:20px;
+}
+
+@media only screen and (max-width:500px) {
+    #traindetailsbox
+    {
+        width:100%;
+        padding:0px;
+    }
+}
+</style>
